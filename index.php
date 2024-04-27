b3a3b5e87569f41b3dcd7787a8b82040aa69596b
Avleen <11avleena@gmail.com>
2024-04-28 02:10:23 +0530

    Login & Sign up

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__ . '/includes/dbh.inc.php';
+
+$request = strtok($_SERVER['REQUEST_URI'], '?'); // Get the request URI without query string
+$pageDir = __DIR__ . '/pages/';
+$apiDir = __DIR__ . '/api/';
+
+session_start();
+
+switch ($request) {
+    // Main pages
+  case '':
+  case '/':
+    require $pageDir . 'round1_ar.php';
+    break;
+  case 'login':
+    require $pageDir . 'login.php';
+    break;
+  case 'signup':
+    require $pageDir . 'signup.php';
+    break;
+      
+  default:
+    http_response_code(404);
+    echo $request;
+    require $pageDir . '404.php';
+}
---
+++ pages/signup.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/style.css">
+    <title>Document</title>
+    <script src="https://unpkg.com/feather-icons"></script>
+</head>
+<body>
+    <div class="form-container">
+        <h1>Welcome</h1>
+        <p id="signup-ref">Already have an account? <a href="login.html">Login</a></p>
+        <form method="POST" action="/api/login">
+            <input class="inpname" type="text" placeholder="Name" id="email">
+            <input class="inp1" type="email" placeholder="E-Mail" id="email" ><br>
+            <input class="inp2" type="password" placeholder="Password"><br>
+            <input class="inp2" type="password" placeholder="Confirm Password"><br>
+        </form>
+        <button type="submit" class="submit">Sign Up</button>
+    </div>
+    <script>
+        feather.replace();
+      </script>
+</body>
+</html>
